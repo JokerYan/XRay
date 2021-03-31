ca5f76cca3ff6f595ee066b37356f743f21d1534
JokerYan <770792058@qq.com>
2021-03-31 12:59:26 +0800

    Add adversarial model

---
+++ lib/models/cw_inf_att.py
@@ -0,0 +1,60 @@
+import torch
+from lib.core.evaluate import cal_accuracy
+
+class CWInfAttack():
+    def __init__(self, model, c, lr, steps, device='cuda'):
+        self.model = model
+        self.c = c
+        self.lr = lr
+        self.steps = steps
+        self.device = device
+
+    def forward(self, images, labels):
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
+        dummy_labels = torch.zeros(images.shape[0])
+        w = self.get_init_w(images).detach()
+        w.requires_grad = True
+
+        tau = 1
+
+        best_adv_images = images.clone().detach()
+        best_L_inf = 1e10*torch.ones((len(images))).to(self.device)
+
+        optimizer = torch.optim.Adam([w], lr=self.lr)
+
+        for step in range(self.steps):
+            adv_images = self.w_to_adv_images(w)
+            _, output_c = self.model(adv_images)
+
+            f_value = self.c * self.get_f_value(output_c)
+            delta = self.w_to_delta(w, images)
+            distance = self.inf_distance(delta, tau)
+            loss = f_value + distance
+
+            # compute gradient and do update step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # print out results
+            acc = cal_accuracy(output_c, dummy_labels)
+            avg_delta = torch.mean(delta)
+            print('Acc: {}\tDelta: {}'.format(acc, avg_delta))
+
+    def get_f_value(self, outputs, src_c=1, target_c=0):
+        f6 = torch.relu(outputs[:, src_c] - outputs[:, target_c])
+        return f6
+
+    def inf_distance(self, delta, tau):
+        dist_vec = torch.relu(delta - tau)
+        return torch.sum(dist_vec)
+
+    def w_to_adv_images(self, w):
+        return 1/2 * (torch.tanh(w) + 1)
+
+    def w_to_delta(self, w, x):
+        return self.w_to_adv_images(w) - x
+
+    def get_init_w(self, x):
+        return torch.atanh(2 * x - 1)

0ad842ca3b4d8290e42762719a1eb0c9f763a816
JokerYan <770792058@qq.com>
2021-05-29 11:29:33 +0800

    scale up to 255 when saving model output image

--- utils/image_preprocessing.py
+++ utils/image_preprocessing.py
@@ -126,6 +126,7 @@ def save_image_to_disk(image, dir, filename):
     if isinstance(image, torch.Tensor):
         image = image.detach().cpu().numpy()
         image = image.transpose((1, 2, 0))
+        image = image * 255
     cv2.imwrite(path, image)
     assert os.path.isfile(path)
     return path

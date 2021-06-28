4228f7edbeadd86989f8608fa203e1955d63e856
JokerYan <770792058@qq.com>
2021-06-28 11:23:04 +0800

    add dummy attack cfg

--- attack.sh
+++ attack.sh
@@ -1,1 +1,1 @@
-python attack.py --testModel hrnetv2_w64_imagenet_pretrained.pth --dataDir C:/Code/Projects/XRay/Data/
+python attack.py --cfg dummy --testModel hrnetv2_w64_imagenet_pretrained.pth --dataDir C:/Code/Projects/XRay/Data/

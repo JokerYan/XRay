9c787df96e7f65a2ba2ac8bba38207a319c94b60
JokerYan <770792058@qq.com>
2021-07-01 10:35:49 +0800

    add visualize transforms

---
+++ debug.py
@@ -0,0 +1,3 @@
+from utils.debug_tools import visualize_transform
+
+visualize_transform()
--- utils/debug_tools.py
+++ utils/debug_tools.py
@@ -5,11 +5,14 @@ import torch
 import glob
 import numpy as np
 from torch.backends import cudnn
+import torchvision.transforms as transforms
 
 from lib import config
 from lib.config import update_config
 from lib.models.xray_net import XRayNet
-from utils.model_loader import Args
+from utils.model_loader import Args, construct_model
+from utils.xray_dataset import XRayDataset
+import utils.image_transforms as custom_transforms
 
 default_debug_dir = os.path.join('.', 'debug')
 
@@ -47,18 +50,42 @@ def clear_debug_image(debug_dir=default_debug_dir):
     print('debug image cleared')
 
 
-# def visualize_transform():
-#     args = Args()
-#     args.cfg = 'experiments/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100_adapted_linux.yaml'
-#     args.testModel = 'hrnetv2_w64_imagenet_pretrained.pth'
-#     update_config(config, args)
-#     model = XRayNet(config)
-#
-#     # cudnn related setting
-#     cudnn.benchmark = config.CUDNN.BENCHMARK
-#     torch.backends.cudnn.deterministic = config.CUDNN.DETERMINISTIC
-#     torch.backends.cudnn.enabled = config.CUDNN.ENABLED
-#     gpus = list(config.GPUS)
-#     model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
-#
-#
+def visualize_transform():
+    cfg_path = 'experiments/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100_adapted_linux.yaml'
+    pretrained_model = 'hrnetv2_w64_imagenet_pretrained.pth'
+    model, args, config = construct_model(cfg_path, pretrained_model)
+    model_state = torch.load(os.path.join(
+        'output/ILSVRC/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100_adapted_linux',
+        'model_best.pth.tar'))
+    model.load_state_dict(model_state)
+    gpus = list(config.GPUS)
+    model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+
+    valid_dataset = XRayDataset(
+        './data/val_image_selected.csv',
+         transforms.Compose([
+             # TODO: Change Random Crop to Centre Crop
+             custom_transforms.ImageToOne(),
+             custom_transforms.MaskToXray(),
+             custom_transforms.ToTensor(cuda=False),
+             custom_transforms.Rescale(int(config.MODEL.IMAGE_SIZE[0])),
+             # custom_transforms.Grayscale(enabled=config.GRAYSCALE),
+             custom_transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225]),
+         ]))
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=config.TEST.BATCH_SIZE_PER_GPU*len(gpus),
+        shuffle=True,
+        num_workers=config.WORKERS,
+        pin_memory=True
+    )
+
+    for i, data in enumerate(valid_loader):
+        if i > 0:
+            break
+        model_input = data['video_frame']
+        target_x = data['mask_frame']
+        target_c = data['is_fake']
+        output_x, output_c = model(model_input)
+

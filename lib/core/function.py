85d5c9eaefb183519dc2f6cc998fd7e4a23750ea
JokerYan <770792058@qq.com>
2021-07-07 16:33:36 +0800

    increase neighbour target mean

--- lib/core/function.py
+++ lib/core/function.py
@@ -410,7 +410,7 @@ def smooth_distill(config, train_loader, model_teacher, model_student, criterion
 
 
 def get_input_neighbour(input_data, grad):
-    target_mean = 0.002
+    target_mean = 0.005
     batch_size = grad.shape[0]
 
     # grad mean calculated based on absolute value

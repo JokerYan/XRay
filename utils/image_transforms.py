b340726b6ec3f2445e8f9af1d79c32678ee72df2
JokerYan <770792058@qq.com>
2021-07-11 11:17:49 +0800

    reduce masked noise variance

--- utils/image_transforms.py
+++ utils/image_transforms.py
@@ -248,7 +248,7 @@ class Noise(object):
 
 
 class MaskedNoise(object):
-    def __init__(self, variance=0.1):
+    def __init__(self, variance=0.05):
         self.variance = variance
 
     def __call__(self, sample):

43eb235b760c209dc93f555961fbbb1102b23789
JokerYan <770792058@qq.com>
2021-07-12 16:22:04 +0800

    debug print xray net ensemble output_c list

--- lib/models/xray_net_ensemble.py
+++ lib/models/xray_net_ensemble.py
@@ -14,6 +14,7 @@ class XRayNetEnsemble(nn.Module):
             output_x, output_c = model(x)
             output_x_list.append(output_x)
             output_c_list.append(output_c)
+        print(output_c_list)
         output_x_mean = torch.mean(torch.stack(output_x_list), dim=0)
         output_c_mean = torch.mean(torch.stack(output_c_list), dim=0)
         return output_x_mean, output_c_mean

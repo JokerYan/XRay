a90c2943a99914941821c41f95006776970cc1f5
JokerYan <770792058@qq.com>
2021-07-12 16:46:16 +0800

    add output_c_variance

--- lib/models/xray_net_ensemble.py
+++ lib/models/xray_net_ensemble.py
@@ -14,7 +14,12 @@ class XRayNetEnsemble(nn.Module):
             output_x, output_c = model(x)
             output_x_list.append(output_x)
             output_c_list.append(output_c)
+        output_x_list = torch.stack(output_x_list)
+        output_c_list = torch.stack(output_c_list)
         print(output_c_list)
-        output_x_mean = torch.mean(torch.stack(output_x_list), dim=0)
-        output_c_mean = torch.mean(torch.stack(output_c_list), dim=0)
-        return output_x_mean, output_c_mean
+
+        output_x_mean = torch.mean(output_x_list, dim=0)
+        output_c_mean = torch.mean(output_c_list, dim=0)
+        # output_c_entropy = torch.distributions.Categorical(probs=output_c_list).entropy()
+        output_c_variance = 1 - torch.var(output_c_list, dim=0)
+        return output_x_mean, output_c_variance

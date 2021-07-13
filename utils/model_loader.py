1a76cddd21e67d2e7c3c5801d47188886107d5fd
JokerYan <770792058@qq.com>
2021-07-13 08:45:55 +0800

    implement construct ensemble model

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,9 +1,12 @@
+import json
+
 import torch
 from torch.backends import cudnn
 
 from lib.config import config
 from lib.config import update_config
 from lib.models.xray_net import XRayNet
+from lib.models.xray_net_ensemble import XRayNetEnsemble
 
 
 class Args:
@@ -32,3 +35,17 @@ def construct_model(cfg_path, pretrained_model):
 
     return model, args, config
 
+
+def construct_ensemble_model(cfg_json_path):
+    config_json = json.load(open(cfg_json_path))
+    model_list = []
+    for model_info in config_json['models']:
+        model, args, target_config = construct_model(model_info['model_cfg_path'], pretrained_model="")
+        state_dict = torch.load(model_info['model_path'])
+        model.load_state_dict(state_dict)
+        model = model.cuda()
+        model_list.append(model)
+    model_ensemble = XRayNetEnsemble(model_list)
+    gpus = [i for i in range(torch.cuda.device_count())]
+    model_ensemble = torch.nn.DataParallel(model_ensemble, device_ids=gpus).cuda()
+    return model_ensemble

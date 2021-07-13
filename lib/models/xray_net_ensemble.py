92106e0f663b8f74af2ae55ac745d6ee1b77fb67
JokerYan <770792058@qq.com>
2021-07-13 20:11:01 +0800

    debug print ensemble entropy

--- lib/models/xray_net_ensemble.py
+++ lib/models/xray_net_ensemble.py
@@ -19,7 +19,9 @@ class XRayNetEnsemble(nn.Module):
 
         output_x_mean = torch.mean(output_x_list, dim=0)
         output_c_mean = torch.mean(output_c_list, dim=0)
-        # output_c_entropy = torch.distributions.Categorical(probs=output_c_list).entropy()
+
+        entropy_threshold = 0.6853
+        output_c_entropy = entropy_threshold - torch.distributions.Categorical(probs=output_c_list).entropy() + 0.5
 
         variance_threshold = 0.03
         output_c_variance = torch.var(output_c_list, dim=0) + (0.5 - variance_threshold)
@@ -29,5 +31,6 @@ class XRayNetEnsemble(nn.Module):
         print(output_c_list.reshape(-1))
         print(output_c_mean)
         print(output_c_variance)
+        print(output_c_entropy)
 
         return output_x_mean, output_c_final

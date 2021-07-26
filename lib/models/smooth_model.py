f03ff8792ddd5c534174194661d73192e7120c66
JokerYan <770792058@qq.com>
2021-07-26 23:17:45 +0800

    use grad data to weight gaussian noise

--- lib/models/smooth_model.py
+++ lib/models/smooth_model.py
@@ -5,7 +5,7 @@ from utils.debug_tools import save_image_stack, clear_debug_image
 
 
 class SmoothModel(nn.Module):
-    def __init__(self, base_model, mean=0, std=0.03, sample_size=20):
+    def __init__(self, base_model, mean=0, std=0.3, sample_size=20):
         super(SmoothModel, self).__init__()
         self.base_model = base_model
         self.mean = mean
@@ -17,11 +17,11 @@ class SmoothModel(nn.Module):
         input_clone.requires_grad = True
         _, base_output = self.base_model(input_clone)
 
-        # torch.max(base_output).backward()
-        # grad_data = input_clone.grad.data
-        # grad_data = torch.abs(grad_data)
-        # grad_data -= grad_data.min(1, keepdim=True).values
-        # grad_data /= grad_data.max(1, keepdim=True).values
+        torch.max(base_output).backward()
+        grad_data = input_clone.grad.data
+        grad_data = torch.abs(grad_data)
+        grad_data -= grad_data.min(1, keepdim=True).values
+        grad_data /= grad_data.max(1, keepdim=True).values
 
         input_dummy = torch.ones(x.shape)
         output_list = []
@@ -29,7 +29,7 @@ class SmoothModel(nn.Module):
         for i in range(self.sample_size):
             gaussian_noise = torch.normal(self.mean * input_dummy, self.std * input_dummy).cuda()
 
-            # gaussian_noise = gaussian_noise * grad_data
+            gaussian_noise = gaussian_noise * grad_data
 
             gaussian_input = x + gaussian_noise
             _, gaussian_output = self.base_model(gaussian_input)
